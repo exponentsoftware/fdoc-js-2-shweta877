976d818ac0626ec4b97780af00cd7a45eb99505a
shweta gupta <shwetaramgupta95@gmail.com>
2021-09-19 18:01:29 +0530

    'array'

---
+++ index.js
@@ -0,0 +1,27 @@
+const countries = [
+  'ALBANIA',
+  'BOLIVIA',
+  'CANADA',
+  'DENMARK',
+  'ETHIOPIA',
+  'FINLAND',
+  'GERMANY',
+  'HUNGARY',
+  'IRELAND',
+  'JAPAN',
+  'KENYA'
+]
+const ArrayOfCountryArray= (countries) =>{
+    var Country=[]
+    var data=[]
+    countries.map((item) =>{
+        data.push(item.charAt(0).toUpperCase()+ item.slice(1).toLowerCase())
+        data.push(item.substring(0,3));
+        data.push(item.length)
+        Country.push(data)
+        data=[]
+
+    })
+    console.log(Country);
+}
+ArrayOfCountryArray(countries) 
